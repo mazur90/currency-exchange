cd9e031c4780c88cfb92e57ba14441d9f3400228
Dawid Mazurek <dawid.mazurek90@gmail.com>
2023-04-28 10:27:38 +0200

    functions added

--- js/script.js
+++ js/script.js
@@ -1,39 +1,58 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+{
+    const calculateResult = (amount, currency) => {
+        const CHF = 4.71;
+        const EUR = 4.64;
+        const USD = 4.25;
+        const GBP = 5.23;
+        switch (currency) {
+            case "CHF":
+                return amount / CHF;
+            case "EUR":
+                return amount / EUR;
+            case "USD":
+                return amount / USD;
+            case "GBP":
+                return amount / GBP;
 
-let CHF = 4.71;
-let EUR = 4.64;
-let USD = 4.25;
-let GBP = 5.23;
+        }
+    };
 
-amountElement.focus();
+        const updateResult = (amount, currency, result) => {
+            const resultElement = document.querySelector(".js-result");
+            resultElement.innerHTML = (`${result.toFixed(2)} ${currency}`);
+        }
+    
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let amount = +amountElement.value;
-    let currency = currencyElement.value;
-    switch (currency) {
-        case "CHF":
-            result = amount / CHF;
-            break;
-        case "EUR":
-            result = amount / EUR;
-            break;
-        case "USD":
-            result = amount / USD;
-            break;
-        case "GBP":
-            result = amount / GBP;
-            break;
-    }
+    const onFormSubmit = (event) => {
+        event.preventDefault();
 
-    resultElement.innerHTML = (`${result.toFixed(2)} ${currency}`);
-});
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+        
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+        
+        const result = calculateResult(amount, currency)
+        
+        updateResult(amount, currency, result)
+    };
 
-let reset = document.querySelector(".js-reset")
-reset.addEventListener("click", () => {
-resultElement.innerHTML = 0
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("submit", onFormSubmit)
+    };
+
+    init()
+    
+    const reset = () => {
+        const reset = document.querySelector(".js-reset")
+        reset.addEventListener("click", resultElement.innerHTML = 0)
+        }
+        
+     reset()
+
 }
-);
+
+
+
+
